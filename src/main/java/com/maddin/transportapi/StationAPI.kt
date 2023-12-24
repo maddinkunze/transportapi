ca1791769179367f3000643a82c1321c07e1f4ae
Martin Kunze <martin@martinkunze.com>
2023-12-25 00:58:51 +0100

    added a static station cache without saving on disk yet (see gh issue #1 for more info); fixed some smaller issues

--- src/main/java/com/maddin/transportapi/StationAPI.kt
+++ src/main/java/com/maddin/transportapi/StationAPI.kt
@@ -1,18 +1,66 @@
 package com.maddin.transportapi
 
+import java.time.Duration
+import java.time.LocalDateTime
+
 interface StationCache {
     fun addSearch(search: String, results: List<Station>)
     fun getSearch(search: String) : List<Station>?
+    fun searchStations(search: String) : List<Station>
+    fun extendSearch(search: String, existing: List<Station>) : List<Station> {
+        val additional = searchStations(search)
+        val extended = existing.toMutableList()
+        for (item in additional) {
+            if (extended.find { it.id == item.id } != null) { continue }
+            extended.add(item)
+        }
+        return extended
+    }
 }
 
-class DefaultStationCache : StationCache {
+@Suppress("NewApi")
+data class CachedStation(val station: Station, var updated: LocalDateTime) {
+    constructor(station: Station) : this(station, LocalDateTime.now())
+    fun update() {
+        updated = LocalDateTime.now()
+    }
+
+    fun isValid(expiresAfter: Duration) : Boolean {
+        return LocalDateTime.now() <= updated + expiresAfter
+    }
+}
+
+open class DefaultStationCache(private val expiresAfter: Duration) : StationCache {
+    @Suppress("NewApi")
+    constructor() : this(Duration.ofDays(7))
     private val sessionCache = mutableMapOf<String, List<Station>>()
+    private val stationCache = mutableListOf<CachedStation>()
     override fun addSearch(search: String, results: List<Station>) {
         sessionCache[search] = results
+
+        for (item in results) {
+            val existing = stationCache.find { it.station.id == item.id }
+            if (existing != null) {
+                existing.update()
+            } else {
+                stationCache.add(CachedStation(item))
+            }
+        }
     }
 
     override fun getSearch(search: String): List<Station>? {
         return sessionCache[search]
+    }
+
+    private fun stationMatchesSearch(station: Station, search: String) : Boolean {
+        return station.name.contains(search, ignoreCase=true)
+    }
+    private fun stationMatchesSearchAndIsNotTooOld(station: CachedStation, search: String) : Boolean {
+        return station.isValid(expiresAfter) && stationMatchesSearch(station.station, search)
+    }
+
+    override fun searchStations(search: String): List<Station> {
+        return stationCache.mapNotNull { if (stationMatchesSearchAndIsNotTooOld(it, search)) { it.station } else { null } }
     }
 }
 
@@ -34,9 +82,9 @@ interface CachedStationAPI : StationAPI {
         }
 
         // if we have cached other stations that would match the search, add them
-        /*if (stationCache != null) {
+        if (stationCache != null) {
             stations = stationCache!!.extendSearch(search, stations)
-        }*/ // TODO
+        }
 
         return stations
     }
